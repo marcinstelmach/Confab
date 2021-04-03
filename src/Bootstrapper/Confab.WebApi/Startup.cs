15377b6bc18713e050f7e51ed731f375e6c2bb7d
Marcin Stelmah <marcinxd4@gmail.com>
2021-04-03 13:50:57 +0200

    Add project files.

---
+++ src/Bootstrapper/Confab.WebApi/Startup.cs
@@ -0,0 +1,31 @@
+namespace Confab.WebApi
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapGet("/", async x => await x.Response.WriteAsync("Hello"));
+            });
+        }
+    }
+}
